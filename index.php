03df81a959936ee692f5e0dcce89b0773b786c99
Alexis Mejias <alme1304@gmail.com>
2012-07-05 13:05:34 -0400

    * fixed the page title

--- index.php
+++ index.php
@@ -5,17 +5,14 @@
 		<?php if(have_posts()): ?>
 		<?php while (have_posts()): the_post();?>
 		<div id="right">
-            
+
 			<div id="summary">
-				<!-- <h1><?php wp_title();?></h1> -->
+				<h1><?php wp_title("",true);?></h1>
 				<?php the_content(''); ?>
 				<?php endwhile; ?>
 			<?php endif; ?>
 			</div>
-				
-				
-			<div id="content"></div>				
-                
-<!--===================END OF ACTUAL SECTIONS ===================-->            
+			<div id="content"></div>
+<!--===================END OF ACTUAL SECTIONS ===================-->
 		</div>
 <?php get_footer(); ?>
