28d852ae86f02fb6045fe2b6457ec34a3b30eb07
Alexis Mejias <alme1304@gmail.com>
2012-07-06 12:02:40 -0400

    * styling changes

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Alexis Mejias</title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>" />
-	<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+	<!-- <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'> -->
+	<link href='http://fonts.googleapis.com/css?family=Dosis:700,200,500' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
 	<meta name="apple-mobile-web-app-capable" content="yes" />
