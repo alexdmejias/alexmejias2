9346db323989bdc2998c79c5aff7ba63ca740774
Alexis Mejias <alme1304@gmail.com>
2012-07-06 17:21:32 -0400

    * no longer uses zocial

--- footer.php
+++ footer.php
@@ -2,10 +2,11 @@
 <footer>
 	<a href="#">Back to Top</a>
 	<h3>Share it</h3>
-	<a href="#" class="zocial twitter">twitter</a>
-	<a href="#" class="zocial facebook">facebook</a>
-	<a href="#" class="zocial googleplus">googleplus</a>
-	<a href="#" class="zocial linkedin">linkedin</a>
+	<a href="#" class="round twitter">twitter</a>
+	<a href="#" class="round facebook">facebook</a>
+	<a href="#" class="round googleplus">googleplus</a>
+	<a href="#" class="round linkedin">linkedin</a>
+	<a href="#" class="round reddit">reddit</a>
 </footer>
 
 
