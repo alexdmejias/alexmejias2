86ad82bd18daeebb8bd9c59de6d516a6504d90a3
Alexis Mejias <alme1304@gmail.com>
2012-07-06 21:17:01 -0400

    * fixed share menu

--- footer.php
+++ footer.php
@@ -2,11 +2,13 @@
 <footer>
 	<a href="#">Back to Top</a>
 	<h3>Share it</h3>
-	<a href="#" class="round twitter">twitter</a>
-	<a href="#" class="round facebook">facebook</a>
-	<a href="#" class="round googleplus">googleplus</a>
-	<a href="#" class="round linkedin">linkedin</a>
-	<a href="#" class="round reddit">reddit</a>
+	<ul id="share">
+		<li><a href="#" class="round twitter">twitter</a></li>
+		<li><a href="#" class="round facebook">facebook</a></li>
+		<li><a href="#" class="round googleplus">googleplus</a></li>
+		<li><a href="#" class="round linkedin">linkedin</a></li>
+		<li><a href="#" class="round reddit">reddit</a></li>
+	</ul>
 </footer>
 
 
