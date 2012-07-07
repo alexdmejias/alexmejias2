453f9b5d047628071f00d60341250ce226ce4066
Alexis Mejias <alme1304@gmail.com>
2012-07-06 23:44:04 -0400

    * styling changes

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
 <!-- </div>end #cont -->
 <footer>
-	<a href="#">Back to Top</a>
-	<h3>Share it</h3>
-	<ul id="share">
-		<li><a href="#" class="round twitter">twitter</a></li>
-		<li><a href="#" class="round facebook">facebook</a></li>
-		<li><a href="#" class="round googleplus">googleplus</a></li>
-		<li><a href="#" class="round linkedin">linkedin</a></li>
-		<li><a href="#" class="round reddit">reddit</a></li>
-	</ul>
+	<a href="#" id="to_top">Back to Top</a>
+	<div id="share_menu">
+		<h3>Share it</h3>
+		<ul id="share">
+			<li><a href="#" class="round twitter">twitter</a></li>
+			<li><a href="#" class="round facebook">facebook</a></li>
+			<li><a href="#" class="round googleplus">googleplus</a></li>
+			<li><a href="#" class="round linkedin">linkedin</a></li>
+			<li><a href="#" class="round reddit">reddit</a></li>
+		</ul>
+	</div>
 </footer>
 
 
