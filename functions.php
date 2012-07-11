92071b1e22ea2d7afd1877511f2b439cc72f8dc0
Alexis Mejias <alme1304@gmail.com>
2012-07-11 16:49:18 -0400

    * closes #24. siwtchded over to minified files

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
    wp_deregister_script('jquery');
    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
-   wp_register_script('script', get_template_directory_uri().'/js/script.js');
+   wp_register_script('script', get_template_directory_uri().'/js/script.min.js');
    wp_enqueue_script('jquery');
    wp_enqueue_script('script');
 
