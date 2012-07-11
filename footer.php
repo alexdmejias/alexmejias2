920e90d0f66671fb1867471c94b8b0c812133a35
Alexis Mejias <alme1304@gmail.com>
2012-07-11 00:36:28 -0400

    * closes #16

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 	<div id="share_menu">
 		<h3>Share it</h3>
 		<ul id="share">
-			<li><a href="#" class="round twitter">twitter</a></li>
-			<li><a href="#" class="round facebook">facebook</a></li>
-			<li><a href="#" class="round googleplus">googleplus</a></li>
-			<li><a href="#" class="round linkedin">linkedin</a></li>
-			<li><a href="#" class="round reddit">reddit</a></li>
+			<li><a href="https://twitter.com/alme1304" class="round twitter">twitter</a></li>
+			<li><a href="https://github.com/amejias101" class="round github">github</a></li>
+			<li><a href="http://www.behance.net/aleximejias" class="round behance">behance</a></li>
+			<li><a href="https://plus.google.com/u/0/117004785307085745469/posts" class="round googleplus">googleplus</a></li>
+			<li><a href="http://www.linkedin.com/profile/view?id=193520953&locale=en_US&trk=tab_pro" class="round linkedin">linkedin</a></li>
 		</ul>
 	</div>
 </footer>
