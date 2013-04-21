a1e6fadaa2abe2db5e835d8ef85fa7a012ed0532
alex mejias <amejias@groupsjr.com>
2013-04-21 10:45:38 -0400

    added 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div id="cont">
+	<?php get_sidebar(); ?>
+	<div id="content">
+		<h2>Sorry but it seems that the page you are looking for does not exist.</h2>
+	</div>
+</div> <!-- end #cont -->
+<?php get_footer(); ?>
